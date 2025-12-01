5cfa64eed45016e77f068d43d290cecffe8a94f8
codingcrew <nick261505@gmail.com>
2025-12-01 21:08:46 +0900

    1201목요일7시or월요일7시

--- 목요일7시/1113/거북이2.py
+++ 목요일7시/1113/거북이2.py
@@ -52,60 +52,40 @@ def 총알생성():
     bullet.penup()
     bullet.setposition(c.xcor(), c.ycor())
     bullets.append(bullet)
+##########################
+# 적 생성
+# [] : 리스트   
+# 
 
-#####################################
-# 적 관련 변수 및 함수
-# 적들을 생성후 담을 리스트
-enemies=[]#brackets
-# 적 내려오는 속도
-enemy_speed=1
-colors=["red","blue","orange","green","yellow"]
-# 적생성 함수
+enemies=[]
+
+colors=["red","blue","yellow","white","orange"]
+
+
 def 적생성():
-    # 확률(1/50)로 적생성
-    if random.randint(1,50) == 1:
-        enemy = turtle.Turtle()
-        enemy.shape("turtle")
-        enemy.color(random.choice(colors))
-        enemy.penup()
-        enemy.setheading(270)
+    #지역변수(local variable)
+    num=random.randint(0,10)
+    if num!=0:
+        return
+    enemy = turtle.Turtle()
+    enemy.shape("turtle")
+    enemy.color(random.choice(colors))
+    enemy.penup()
+    enemy.setposition(random.randint(-380,380),300)
+    enemy.dy=random.randint(2,4)
+    #만들어진 적을 리스트 enemies[] 추가
+    enemies.append(enemy)
 
-        x_pos=random.randint(-380,380)
-        enemy.setposition(x_pos, 300)
-        enemies.append(enemy)
-#적이동 함수        
 def 적이동():
     for enemy in enemies[:]:
-        y = enemy.ycor() - enemy_speed
-        enemy.sety(y)
-        enemy.left(45)
-
-        # 화면 바닥에 닿으면 삭제
-        if y < -300:
+        y=enemy.ycor() - enemy.dy#변경될 y축값
+        enemy.sety(y)#y축변경
+        #제어문
+        if y < -200:
             enemy.hideturtle()
             enemies.remove(enemy)
-###################################
-# 총알 적 충돌처리
-def 충돌체크():
-    #점수처리는 나중에
-    #모든 총알 과 모든 적을 비교:
-    for bullet in bullets[:]:
-        for enemy in enemies[:]:
-            #두 물체 사이의 거리가 20미만이면 충돌로 간주
-            if bullet.distance(enemy) < 20:
 
-                #1. 적 제거
-                enemy.hideturtle()
-                if enemy in enemies: 
-                    enemies.remove(enemy)
 
-                #2. 총알 제거
-                bullet.hideturtle()
-                if bullet in bullets: 
-                    bullets.remove(bullet)
-
-                #3. 점수 증가(나중에) 
-                break
 
 
 s.listen()
@@ -118,7 +98,7 @@ while True:
     총알이동()
     적생성()
     적이동()
-    충돌체크()
+    #충돌체크()
     s.update()
     time.sleep(0.02)#100f
 
